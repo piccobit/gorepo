58e094066169be2acc70e4776ecf7e7a31e5bc35
Hans-Dieter Stich <hans-dieter.stich@continental-corporation.com>
2019-08-05 17:08:33 +0200

    Adding Golang extension for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,10 @@ author = 'HD Stich'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['autoapi.extension']
+extensions = [
+  'sphinxcontrib_golangdomain',
+  'autoapi.extension'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
