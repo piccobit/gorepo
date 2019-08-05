6d049c1769d7aea6e96101a2821a8d9b2ef2cac0
Hans-Dieter Stich <hans-dieter.stich@continental-corporation.com>
2019-08-05 16:18:55 +0200

    Fixed extension setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'HD Stich'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions.append('autoapi.extension')
+extensions = ['autoapi.extension']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
